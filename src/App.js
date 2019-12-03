ef7997a8052fdc8bcac116dfb42434bd7430d4bb
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-12-03 16:22:49 +0530

    set block design with table structure to set up the number in blocks

--- src/App.js
+++ src/App.js
@@ -11,13 +11,18 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 d-flex justify-content-center align-items-center">
-              <table className="anstable" style={{ width: "50%", border: "2px solid white" }}>
+        <div className="container d-flex justify-content-center align-items-center">
+          <div className="row d-flex justify-content-center align-items-center">
+            <div className="col-md-12 ">
+              <h3 className="text-center text-white">Arrange puzzle in numeric order</h3><br />
+              <table className="d-flex justify-content-center" width="400px">
                 <tbody>
-                  {[...Array(5)].map((data, index) => {
-                    return <tr><td>&nbsp;</td></tr>
+                  {[...Array(3)].map((data, index) => {
+                    return <tr>
+                      {[...Array(3)].map((data, index) => {
+                            return <td width="130px" height="130px">&nbsp;</td>
+                          })}
+                    </tr>
                   })}
                 </tbody>
               </table>
